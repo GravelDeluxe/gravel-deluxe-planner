4e87effabc8742c03cef8baf308523618bcd7804
DerRemo <35336907+DerRemo@users.noreply.github.com>
2026-07-08 23:03:30 +0200

    chore: scaffold gravel-planner (package.json, config)

---
+++ js/config.js
@@ -0,0 +1,12 @@
+export const BROUTER_BASE = 'https://brouter.de/brouter';
+export const DEFAULT_PROFILE = 'gravel';
+export const FALLBACK_PROFILE = 'trekking';
+
+export const TILE_URL = 'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png';
+export const TILE_ATTRIBUTION =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>-Mitwirkende | Kacheln: <a href="https://www.cyclosm.org">CyclOSM</a>';
+
+export const NOMINATIM_BASE = 'https://nominatim.openstreetmap.org/search';
+
+export const MAP_START = [51.163, 10.447]; // Mitte Deutschlands
+export const MAP_START_ZOOM = 6;
